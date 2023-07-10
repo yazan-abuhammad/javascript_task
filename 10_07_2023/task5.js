f72b8f31ec800caba7498ed15801012f1709b632
yazan-abuhammad <yazanjamalabuhammad@gmail.com>
2023-07-11 00:08:08 +0300

    u

--- 10_07_2023/task5.js
+++ 10_07_2023/task5.js
@@ -1,11 +1,11 @@
-let number = 1;
+let num = 1;
 
 for (let i = 1; i <= 4; i++) {
   let row = "";
   
   for (let j = 1; j <= i; j++) {
-    row += number + " ";
-    number++;
+    row += num + " ";
+    num++;
   }
   
   console.log(row);

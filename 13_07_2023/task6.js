809fcd8c1bb5c5044f1c7d17eda6f22727b1f276
yazan-abuhammad <yazanjamalabuhammad@gmail.com>
2023-08-03 01:28:54 +0300

    ok

--- 13_07_2023/task6.js
+++ 13_07_2023/task6.js
@@ -1,12 +1,12 @@
-
-
+let country = document.querySelector('#country')
+let img= document.querySelector('#pic')
  function changeImg() {
- let country = document.querySelector('#country')
- let img= document.querySelector('#pic')
+ 
+ 
  img.src = "../resorces/" + country.options[country.selectedIndex].value;
- console.log(country)
+
  }
- let country = document.querySelector('#country')
+ 
  country.addEventListener('change' , changeImg)
 
  
--- 13_07_2023/task8.js
+++ 13_07_2023/task8.js
@@ -1,17 +1,16 @@
+let color = "";
+let num = 0;
 
-let color = ''
-let num=0
+document.querySelector(".square").addEventListener("click", function () {
+  color = ["red", "yellow ", "black", "green", "orange", "brown"];
+  document.querySelector(".square").style.backgroundColor = color[num];
+  num++;
+  if (num >= color.length) {
+    num = 0;
+  }
+});
+document.querySelector(".square").addEventListener("mouseout", function () {
+  document.querySelector(".square").style.backgroundColor = "blue";
+});
 
-
-document.querySelector('.square' ).addEventListener('click',function(){
-color=['red','yellow ','black','green','orange','brown']
-    document.querySelector('.square').style.backgroundColor= color[num]
-    num++
-    if (num>5){
-        num=0
-    }
-})
-document.querySelector('.square' ).addEventListener('mouseout',function(){
-
-    document.querySelector('.square').style.backgroundColor= 'blue'
-})
+console.log(Math.floor(Math.random() * 5));
